7993adeb242f04a9a10991c6437221f0205babac
SuhaniMalhotra09 <suhanimalhotra0903@gmail.com>
2025-10-27 18:32:04 +0530

    Create q3.cpp

---
+++ assignment4/q3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+void interleaveQueue(queue<int>& q) {
+    int n = q.size();
+    if (n % 2 != 0) {
+        cout << "Queue has odd number of elements.\n";
+        return;
+    }
+
+    queue<int> firstHalf;
+    for (int i = 0; i < n / 2; i++) {
+        firstHalf.push(q.front());
+        q.pop();
+    }
+
+    while (!firstHalf.empty()) {
+        q.push(firstHalf.front());
+        firstHalf.pop();
+        q.push(q.front());
+        q.pop();
+    }
+}
+
+int main() {
+    queue<int> q;
+    q.push(4);
+    q.push(7);
+    q.push(11);
+    q.push(20);
+    q.push(5);
+    q.push(9);
+
+    cout << "Original Queue: 4 7 11 20 5 9\n";
+    interleaveQueue(q);
+    cout << "Interleaved Queue: ";
+    while (!q.empty()) {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    return 0;
+}
