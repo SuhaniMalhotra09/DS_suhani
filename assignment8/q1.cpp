84a2090641095128ac54bb22bb1e130d819114f2
SuhaniMalhotra09 <suhanimalhotra0903@gmail.com>
2025-11-19 23:58:31 +0530

    Update q1.cpp

--- assignment8/q1.cpp
+++ assignment8/q1.cpp
@@ -1,1 +1,67 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+class BinaryTree {
+public:
+    Node* buildTree() {
+        int val;
+        cin >> val;
+
+        if (val == -1) return NULL;
+
+        Node* root = new Node(val);
+        root->left = buildTree();
+        root->right = buildTree();
+        return root;
+    }
+
+    void preorder(Node* root) {
+        if (!root) return;
+        cout << root->data << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+
+    void inorder(Node* root) {
+        if (!root) return;
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
+
+    void postorder(Node* root) {
+        if (!root) return;
+        postorder(root->left);
+        postorder(root->right);
+        cout << root->data << " ";
+    }
+};
+
+int main() {
+    BinaryTree bt;
+    Node* root = bt.buildTree();
+
+    bt.preorder(root);
+    cout << endl;
+
+    bt.inorder(root);
+    cout << endl;
+
+    bt.postorder(root);
+    cout << endl;
+
+    return 0;
+}
 
